6546f5f64ca3f512e5ab3040cde5528b86811e11
Sahitya Sakthivel <sahitya.sakthivel@kcl.ac.uk>
2022-09-26 16:50:10 +0100

    Completed adding prime numbers to list task and checked against testcases using pytest

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    print("h")
     list = []
+
+    if (number_of_primes >= 1):
+        list.append(2)
+    counter = 3
+    while(len(list) !=  number_of_primes):
+        prime = True
+        for i in range(2,counter):
+            if(counter % i == 0):
+                prime = False
+        if prime:
+            list.append(counter)
+
+        counter+= 1
     return list
